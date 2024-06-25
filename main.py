f22630f1cc5adf56578f0fd8de5c4ded3bfba7e5
Gavin Xue <gavinxue050705@gmail.com>
2024-06-25 11:44:44 -0400

    Working link checking

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from modules import Page
 
 url = "https://uwaterloo.ca/civil-environmental-engineering-information-technology"
 
-page = requests.get(url)
+homepage = requests.get(url)
 
-soup = BeautifulSoup(page.text, 'html.parser')
+soup = BeautifulSoup(homepage.text, 'html.parser')
 
+# scraping pages
 menu_links = [Page('Home', url)]
 
 nav = soup.find('nav', {'class': 'uw-header__nav'})
@@ -18,8 +19,36 @@ for s in subnav:
     links = s.find_all('a', {'class': 'menu__link'})
 
     for a in links:
-        menu_links.append(Page(a.text.strip(), a['href'].strip()))
+        menu_links.append(Page(a.text.strip(), "https://uwaterloo.ca" + a['href'].strip()))
 
-for x in menu_links:
-    print(x.name)
-    print(x.url)
+
+# checking pages
+for menu_link in menu_links:
+    # print(menu_link.name)
+    # print(menu_link.url)
+    page = requests.get(menu_link.url)
+    page_soup = BeautifulSoup(page.text, 'html.parser')
+    main = page_soup.find('main')
+    links = main.find_all('a')
+
+    for link in links:
+        if 'href' not in link.attrs:
+            print('ERROR - NO HREF FOUND')
+            continue
+        if "https://" not in link['href']:
+            print('ERROR - MUST USE ABSOLUTE URL')
+            continue
+
+        try:
+            res = requests.get(link['href'])
+            status = str(res.status_code).strip()
+            print('HTTP ' + status)
+        except requests.exceptions.HTTPError as errh:
+            print ("Http Error:",errh)
+        except requests.exceptions.ConnectionError as errc:
+            print ("Error Connecting:",errc)
+        except requests.exceptions.Timeout as errt:
+            print ("Timeout Error:",errt)
+        except requests.exceptions.RequestException as err:
+            print ("OOps: Something Else",err)
+
