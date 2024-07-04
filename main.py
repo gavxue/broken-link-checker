7e535e35eb8831953df36c2d7af175da8bbd5677
Gavin Xue <gavinxue050705@gmail.com>
2024-07-04 14:54:43 -0400

    JSON from python working

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
 class Page:
     def __init__(self, name, url):
         self.name = name
         self.url = url
+
+    def toJSON(self):
+        return json.dumps(
+            self,
+            default=lambda o: o.__dict__, 
+            sort_keys=True,
+            indent=4)
 
 url = "https://uwaterloo.ca/civil-environmental-engineering-information-technology"
 
@@ -13,7 +21,8 @@ homepage = requests.get(url)
 soup = BeautifulSoup(homepage.text, 'html.parser')
 
 # scraping pages
-menu_links = [Page('Home', url)]
+# menu_links = [(Page('Home', url)).toJSON()]
+menu_links = [{"name": "home", "url": url}]
 
 nav = soup.find('nav', {'class': 'uw-header__nav'})
 subnav = nav.find_all('ul', {'class': 'menu__subnav'})
@@ -22,13 +31,19 @@ for s in subnav:
     links = s.find_all('a', {'class': 'menu__link'})
 
     for a in links:
-        menu_links.append(Page(a.text.strip(), "https://uwaterloo.ca" + a['href'].strip()))
+        # menu_links.append(Page(a.text.strip(), "https://uwaterloo.ca" + a['href'].strip()).toJSON())
+        menu_links.append({"name": a.text.strip(), "url": "https://uwaterloo.ca" + a['href'].strip()})
 
+print(menu_links)
+
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(menu_links, f)
 
 # checking pages
-for menu_link in menu_links:
-    print(menu_link.name)
-    print(menu_link.url)
+# for menu_link in menu_links:
+#     print(menu_link.name)
+#     print(menu_link.url)
+    
     # page = requests.get(menu_link.url)
     # page_soup = BeautifulSoup(page.text, 'html.parser')
     # main = page_soup.find('main')
