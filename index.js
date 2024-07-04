fbe05f8b5912c8626bea077aca0571bf881442dd
Gavin Xue <gavinxue050705@gmail.com>
2024-07-04 14:21:34 -0400

    Reset app

--- index.js
+++
@@ -1,7 +0,0 @@
-
-function getInput() {
-    const input = document.querySelector('#url')
-    const value = input.value
-    console.log(value)
-}
-
--- main.py
+++ main.py
@@ -27,31 +27,31 @@ for s in subnav:
 
 # checking pages
 for menu_link in menu_links:
-    # print(menu_link.name)
-    # print(menu_link.url)
-    page = requests.get(menu_link.url)
-    page_soup = BeautifulSoup(page.text, 'html.parser')
-    main = page_soup.find('main')
-    links = main.find_all('a')
+    print(menu_link.name)
+    print(menu_link.url)
+    # page = requests.get(menu_link.url)
+    # page_soup = BeautifulSoup(page.text, 'html.parser')
+    # main = page_soup.find('main')
+    # links = main.find_all('a')
 
-    for link in links:
-        if 'href' not in link.attrs:
-            print('ERROR - NO HREF FOUND')
-            continue
-        if "https://" not in link['href']:
-            print('ERROR - MUST USE ABSOLUTE URL')
-            continue
+    # for link in links:
+    #     if 'href' not in link.attrs:
+    #         print('ERROR - NO HREF FOUND')
+    #         continue
+    #     if "https://" not in link['href']:
+    #         print('ERROR - MUST USE ABSOLUTE URL')
+    #         continue
 
-        try:
-            res = requests.get(link['href'])
-            status = str(res.status_code).strip()
-            print('HTTP ' + status)
-        except requests.exceptions.HTTPError as errh:
-            print ("Http Error:",errh)
-        except requests.exceptions.ConnectionError as errc:
-            print ("Error Connecting:",errc)
-        except requests.exceptions.Timeout as errt:
-            print ("Timeout Error:",errt)
-        except requests.exceptions.RequestException as err:
-            print ("OOps: Something Else",err)
+    #     try:
+    #         res = requests.get(link['href'])
+    #         status = str(res.status_code).strip()
+    #         print('HTTP ' + status)
+    #     except requests.exceptions.HTTPError as errh:
+    #         print ("Http Error:",errh)
+    #     except requests.exceptions.ConnectionError as errc:
+    #         print ("Error Connecting:",errc)
+    #     except requests.exceptions.Timeout as errt:
+    #         print ("Timeout Error:",errt)
+    #     except requests.exceptions.RequestException as err:
+    #         print ("OOps: Something Else",err)
 
