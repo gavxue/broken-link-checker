aaa36c794aaacaa21c97ffb6321ec0fe588feaff
Gavin Xue <g3xue@uwaterloo.ca>
2024-07-11 15:14:35 -0400

    Added basic styles

--- app.py
+++ app.py
@@ -28,33 +28,36 @@ def check_page(url, event):
 
     for link in links:
         line = link.text.strip() + ' --- '
+        status = "danger"
 
         if 'href' not in link.attrs:
             line += 'ERROR (NO HREF FOUND)'
-            continue
-        if 'mailto:' in link['href']:
+        elif 'mailto:' in link['href']:
             line += 'MAIL LINK (NEEDS MANUAL CHECK)'
-            continue
-        if "https://" not in link['href']:
-            line += 'ERROR - MUST USE ABSOLUTE URL'
-            continue
-            # link['href'] = 'https://uwaterloo.ca' + link['href']
+            status = 'warning'
+        else:
+            if "https://" not in link['href']:
+                # line += 'ERROR - MUST USE ABSOLUTE URL'
+                # continue
+                link['href'] = 'https://uwaterloo.ca' + link['href']
 
-        try:
-            res = requests.get(link['href'], timeout=10)
-            status = str(res.status_code).strip()
-            line += 'HTTP ' + status
-        except requests.exceptions.HTTPError as errh:
-            line += "HTTP ERROR"
-        except requests.exceptions.ConnectionError as errc:
-            line += "ERROR CONNECTING"
-        except requests.exceptions.Timeout as errt:
-            line += "TIMEOUT ERROR"
-        except requests.exceptions.RequestException as err:
-            line += "UNKNOWN ERROR"
+            try:
+                res = requests.get(link['href'], timeout=10)
+                status_code = str(res.status_code).strip()
+                line += 'HTTP ' + status_code
+                if status_code == '200':
+                    status = 'success'
+            except requests.exceptions.HTTPError:
+                line += "HTTP ERROR"
+            except requests.exceptions.ConnectionError:
+                line += "ERROR CONNECTING"
+            except requests.exceptions.Timeout:
+                line += "TIMEOUT ERROR"
+            except requests.exceptions.RequestException:
+                line += "UNKNOWN ERROR"
 
         # log link status
-        socketio.emit('my_response', {'data': line})
+        socketio.emit('response', {'message': line, 'class': status})
 
         if not event.is_set():
             return False
@@ -79,74 +82,19 @@ def background_thread(event):
             for menu_item in menu_items:
                 # log section
                 menu_item_title = menu_item.find('a').text.strip()
-                socketio.emit('my_response', {'data': menu_item_title})
+                socketio.emit('response', {'message': menu_item_title, 'class': 'section'})
                 submenu_items = menu_item.find_all('a', href=True)
                 
                 for submenu_item in submenu_items:
                     # log section item
-                    socketio.emit('my_response', {'data': submenu_item.text.strip()})
+                    socketio.emit('response', {'message': submenu_item.text.strip(), 'class': 'page'})
                     live = check_page('https://uwaterloo.ca' + submenu_item.get('href').strip(), event)
 
                     if not live:
                         return
             
             # log completion
-            socketio.emit('my_response', {'data': 'Success!'})
-
-
-            # links = nav.find_all('a', {'class': 'menu__link'})
-            # for a in links:
-            #     try:
-            #         menu_links.append(Page(a.text.strip(), "https://uwaterloo.ca" + a['href'].strip())) 
-            #     except:
-            #         continue
-
-            # subnav = nav.find_all('ul', {'class': 'menu__subnav'})
-
-            # for s in subnav:
-            #     links = s.find_all('a', {'class': 'menu__link'})
-
-                # for a in links:
-                #     menu_links.append(Page(a.text.strip(), "https://uwaterloo.ca" + a['href'].strip()))
-
-            # for menu_link in menu_links:
-            #     socketio.emit('my_response', {'data': menu_link.name})    
-                # page = requests.get(menu_link.url)
-                # page_soup = BeautifulSoup(page.text, 'html.parser')
-                # main = page_soup.find('main')
-                # links = main.find_all('a')
-
-                # for link in links:
-                #     line = link.text.strip() + ' --- '
-
-                #     if 'href' not in link.attrs:
-                #         line += 'ERROR (NO HREF FOUND)'
-                #         continue
-                #     if 'mailto:' in link['href']:
-                #         line += 'MAIL LINK (NEEDS MANUAL CHECK)'
-                #         continue
-                #     if "https://" not in link['href']:
-                #         line += 'ERROR - MUST USE ABSOLUTE URL'
-                #         continue
-                #         # link['href'] = 'https://uwaterloo.ca' + link['href']
-
-                #     try:
-                #         res = requests.get(link['href'], timeout=10)
-                #         status = str(res.status_code).strip()
-                #         line += 'HTTP ' + status
-                #     except requests.exceptions.HTTPError as errh:
-                #         line += "HTTP ERROR"
-                #     except requests.exceptions.ConnectionError as errc:
-                #         line += "ERROR CONNECTING"
-                #     except requests.exceptions.Timeout as errt:
-                #         line += "TIMEOUT ERROR"
-                #     except requests.exceptions.RequestException as err:
-                #         line += "UNKNOWN ERROR"
-
-                #     socketio.emit('my_response', {'data': line})
-
-                #     if not event.is_set():
-                #         return
+            socketio.emit('response', {'message': 'Success!'})
     finally:
         event.clear()
         thread = None
@@ -166,30 +114,13 @@ def results():
 
 @socketio.event
 def my_event(message):
-    emit('my_response',
-         {'data': message['data']})
+    emit('response',
+         {'message': message['data']})
 
 
 @socketio.event
 def connect():
-    emit('my_response', {'data': 'Connected'})
-
-    # global menu_links
-
-    # homepage = requests.get(url)
-
-    # soup = BeautifulSoup(homepage.text, 'html.parser')
-
-    # # scraping pages
-    # menu_links = [(Page('Home', url))]
-    # nav = soup.find('nav', {'class': 'uw-header__nav'})
-    # subnav = nav.find_all('ul', {'class': 'menu__subnav'})
-
-    # for s in subnav:
-    #     links = s.find_all('a', {'class': 'menu__link'})
-
-    #     for a in links:
-    #         menu_links.append(Page(a.text.strip(), "https://uwaterloo.ca" + a['href'].strip()))
+    emit('response', {'message': 'Connected'})
 
     global thread
     with thread_lock:
